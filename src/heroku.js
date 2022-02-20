c9da61290d81268be5be2f93cefcc64c7d3bc42e
Suzanne DUHAMEL <suzanneduhamel@MacBook-Pro-de-Suzanne-2.local>
2022-02-20 21:39:38 +0100

    v12

--- src/heroku.js
+++ src/heroku.js
@@ -1,7 +1,1 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "https://marvel-back-suz.herokuapp.com/",
-});
-
-export default instance;
+// lien heroku à copier coller à la place de local host
